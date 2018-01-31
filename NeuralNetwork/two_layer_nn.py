7284ddb7d821c57a5f1cdcef3a1c3859aff2530d
Soo <simonjisu@gmail.com>
2018-01-31 14:17:52 +0900

    add workings

--- NeuralNetwork/two_layer_nn.py
+++ NeuralNetwork/two_layer_nn.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 import collections
-import sys
-sys.path.insert(0, '../GD/')
-from GD_common import numerical_gradient
-from layers import *
+from common.layers import *
 
 class TwoLayer(object):
-    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std='sigmoid'):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
         self.params = {}
-        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
-        self.params['b1'] = weight_init_std * np.random.randn(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
-        self.params['b2'] = weight_init_std * np.random.randn(output_size)
+        self.__init_weight(weight_init_std)
 
         # 계층 생성
         self.layers = collections.OrderedDict()
@@ -20,6 +17,27 @@ class TwoLayer(object):
         self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
 
         self.lastLayer = SoftmaxWithLoss()
+
+    def __init_weight(self, weight_init_std):
+        """가중치 초기화
+
+        Parameters
+        ----------
+        weight_init_std : 가중치의 표준편차 지정（e.g. 0.01）
+            'relu'나 'he'로 지정하면 'He 초깃값'으로 설정
+            'sigmoid'나 'xavier'로 지정하면 'Xavier 초깃값'으로 설정
+        """
+        all_size_list = [self.input_size] + [self.hidden_size] + [self.output_size]
+        for idx in range(1, len(all_size_list)):
+            scale = weight_init_std
+            if str(weight_init_std).lower() in ('relu', 'he'):
+                scale = np.sqrt(2.0 / all_size_list[idx - 1])  # ReLU를 사용할 때의 권장 초깃값
+            elif str(weight_init_std).lower() in ('sigmoid', 'xavier'):
+                scale = np.sqrt(1.0 / all_size_list[idx - 1])  # sigmoid를 사용할 때의 권장 초깃값
+            self.params['W' + str(idx)] = scale * np.random.randn(all_size_list[idx - 1], all_size_list[idx])
+            self.params['b' + str(idx)] = np.zeros(all_size_list[idx])
+
+
 
     def predict(self, x):
         for layer in self.layers.values():
@@ -39,17 +57,6 @@ class TwoLayer(object):
 
         acc = np.sum(y == t) / float(x.shape[0])
         return acc
-
-    def num_gradient(self, x, t):
-        loss_function = lambda W: self.loss(x, t)
-
-        grads = {}
-        grads['W1'] = numerical_gradient(loss_function, self.params['W1'])
-        grads['b1'] = numerical_gradient(loss_function, self.params['b1'])
-        grads['W2'] = numerical_gradient(loss_function, self.params['W2'])
-        grads['b2'] = numerical_gradient(loss_function, self.params['b2'])
-
-        return grads
 
     def gradient(self, x, t):
         # forward
